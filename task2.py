275a9b54ebf17ddf9a80a2f85df0386b0f430f12
Muhammad Ali Riaz <muhammadaliriaz6055@gmail.com>
2025-02-06 21:52:06 +0500

    initial commit

--- task2.py
+++ task2.py
@@ -2,24 +2,18 @@ import numpy as np
 import cv2
 
 def connected_component_analysis(image):
-    h, w = image.shape
+    h, w = image.shape[:2]
     labels = np.zeros((h, w), dtype=np.uint8)
     label = 1
     equivalence = {}
-
     for i in range(h):
         for j in range(w):
-            if image[i, j] == 255:  # Foreground pixel
+            if image[i, j] == 255:
                 neighbors = []
-
-                # Check top neighbor
                 if i > 0 and labels[i - 1, j] > 0:
                     neighbors.append(int(labels[i - 1, j]))
-
-                # Check left neighbor
                 if j > 0 and labels[i, j - 1] > 0:
                     neighbors.append(int(labels[i, j - 1]))
-
                 if not neighbors:
                     labels[i, j] = label
                     equivalence[label] = {label}
@@ -27,28 +21,22 @@ def connected_component_analysis(image):
                 else:
                     min_label = min(neighbors)
                     labels[i, j] = min_label
-
+                    union_set = set()
                     for neighbor_label in neighbors:
-                        equivalence[min_label].update(equivalence[neighbor_label])
-                        equivalence[neighbor_label] = equivalence[min_label]
-
-    # Second pass: Resolve equivalence classes
+                        union_set = union_set.union(equivalence[neighbor_label])
+                    for lab in union_set:
+                        equivalence[lab] = union_set
     for i in range(h):
         for j in range(w):
             if labels[i, j] > 0:
                 labels[i, j] = min(equivalence[labels[i, j]])
-
     return labels, equivalence
-
 
 image_path = "cc.png"
 binary_image = cv2.imread(image_path, 0)
-
-labels_loops, equivalence_dict_loops = connected_component_analysis(binary_image)
-
-output_path_loops = "labeled_cc.png"
-cv2.imwrite(output_path_loops, labels_loops*255)
-
-unique_labels = np.unique(labels_loops)
+labels_matrix, equivalence_dict_loops = connected_component_analysis(binary_image)
+output_path = "labeled_cc.png"
+cv2.imwrite(output_path, labels_matrix * 255)
+unique_labels = np.unique(labels_matrix)
 unique_components = unique_labels[unique_labels != 0]
 print(f"Number of connected components: {len(unique_components)}")
