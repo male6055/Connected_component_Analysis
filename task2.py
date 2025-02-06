6498fa3165ef24d94e336b7f6b1d2b325a0b02a5
Muhammad Ali Riaz <muhammadaliriaz6055@gmail.com>
2025-02-06 11:22:01 +0500

    initial commit

--- task2.py
+++ task2.py
@@ -7,3 +7,11 @@ cv2.imshow('original',my_image)
 h,w = my_image.shape[:2]
 
 out_image = np.zeros((h,w),dtype = np.uint8)
+
+for i in range(h):
+    for j in range(w):
+        p = my_image[i,j]
+        if p==255:
+            out_image[i,j]=
+
+
